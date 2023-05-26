06799357fb8ab1a2e593c4d48b78c762e5a4a3e7
Yuriy Petrov <Jurijp2005@gmail.com>
2023-05-26 12:53:34 +0200

    first commit

---
+++ tryparse/tryparse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+bool correct = false;
+string answer = "18";
+int results;
+
+
+Console.WriteLine("How old is Yuriy? In numbers plz:) \n Birthday 2005/04/08");
+
+
+while (correct == false)
+{
+   answer = Console.ReadLine();
+   correct = int.TryParse(answer, out results);
+   
+   if (results != 18)
+   {
+    Console.WriteLine("Wrong Answer, try again");
+    
+   }
+}
+
+Console.ReadLine();
